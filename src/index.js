d59eef78214b72741ef0edf873175605582ef14a
Helena Belicheva <bbelht@gmail.com>
2022-01-08 08:07:57 +0500

    solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
-}
+    var units = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    
+        if (number < 20) return units[number];
+        var num = number % 10;
+        if (number < 100) return tens[~~(number / 10) -2] + (num ? " " + units[num] : "");
+        if (number < 1000) return units[~~(number / 100)] + " hundred" + (number % 100 == 0? "" : " " + toReadable(number % 100));
+        return toReadable(~~(number / 1000)) + " thousand" + (number % 1000 != 0? " " + toReadable(number % 1000) : "");
+    }
